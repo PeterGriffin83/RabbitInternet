0e32b5eadcdccdddba0211203b72884e736b50b8
Peter <peter@griffin.org>
2016-04-24 15:34:34 +0700

    Implement:
    1. Abstract Twitter handling code into a separate module (in Vendors/RabbitInternet/TweetMaps), and update IndexController to communicate with it
    2. Update Index and Layout views to handle google maps
    3. Update globals.php (config/autoload/global) with twitter and cache (testing) defaults
    4. Updated IndexController to communicate with Twitter and pass the retrieved values to the TweetMaps class for processing.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,50 @@
  */
 
 return array(
-    // ...
+    'twitter' => array('access_token' => array(
+                                'token'  => '2328788546-NYgGUjtO7QriMk1rimRwqbhraWStmlEYoZo9xLM',
+                                'secret' => 'F0OVBd37Mr8dnJ2wmWopfob3jnRyYfe2pYJ9bEo2bInP7',
+                            ),
+                            'oauth_options' => array(
+                                'consumerKey' => '6WejwQpq8ykDHHJLwqHpXvHjS',
+                                'consumerSecret' => 'SoBqg9gU4xyAPYv6GUqlQHcz3t0VWQxBYiFpL2wzGUlhBAhRDj',
+                            ),
+                            'http_client_options' => array(
+                                'adapter' => 'Zend\Http\Client\Adapter\Curl',
+                                'curloptions' => array(
+                                    CURLOPT_SSL_VERIFYHOST => false,
+                                    CURLOPT_SSL_VERIFYPEER => false,
+                               ),
+                            ),
+            ),
+    'defaults' => array('twitter' => array(
+                                        'radius' => '50km',
+                                        'count' => 20),
+                        'map' => array(
+                                    'default_city' => 'Bangkok, Thailand',
+                                    'default_city_lat' => '13.7563',
+                                    'default_city_long' => '100.5018')
+                       ),
+    'service_manager' => array(
+	'factories' => array(
+		'cache' => function () {
+			return \Zend\Cache\StorageFactory::factory(array(
+				'adapter' => array(
+					'name' => 'filesystem',
+					'options' => array(
+					'cache_dir' => 'data/cache',
+					'ttl' => 3600, // Set for 1hr
+					),
+				),
+				'plugins' => array(
+					'exception_handler' => array(
+						'throw_exceptions' => false
+					),
+					'Serializer', // dont forget this one as on my example im caching an array
+				),
+			));
+		},
+	),			
+),
+    
 );
--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -6,16 +6,64 @@
  * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
-
+ 
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Http\Request;
+use Zend\Http\Client;
+use Zend\Stdlib\Parameters;
+use ZendService\Twitter\Twitter;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\EventManager\EventManagerAware;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        /**
+         * @author  Peter Griffin
+         * @version 1.0
+         *
+         *  This function is the main entry point for the Index Controller, and it controlls all of the logic in our Single Page App. 
+         * 
+         *  To reduce reading complexity and to adhere to the guidelines of this test, I've moved parts of the logic into separate modules,
+         *  particularly the RabbitInternet module under the vendors folder
+         *
+         *  This function does the following:
+         *  1. Pulls configuration values from the global.php file (config/autoload/global.php) and applies them to the Twitter Map object
+         *  2. Checks if the address parameter was set via a post request (i.e, checking if a User has supplied a city).
+         *     If the city is blank, or has not been entered, the default city (configurable in the aforementioned file) is used instead.
+         *
+         *  3. Checks the cache (configurable in global.php) and if it has data in it, uses it. If not, attempts to communicate to Twitter and get
+         *     geocoded tweet data
+         *
+         *  4. Parses this live data (or returns cached data) and sends it to the main view for output to the screen. 
+         **/
+
+    	$config = $this->getServiceLocator()->get('Config'); // Get configuration and pass to Twitter Class
+        $tweetMap = new \RabbitInternet\TwitterMap\TwitterMap();
+        $tweetMap->configure($config);
+
+
+        $address = $this->getRequest()->getPost('address',$tweetMap->get_default_city());
+        if(empty($address)||$address=='') {
+            $address = $tweetMap->get_default_city();
+        }
+        
+        if (strpos($address, ',') !== false) {
+            $address = explode(',', $address)[0];
+        };
+
+        
+        if($tweetMap->verifyCredentials()) {
+            // Pass Error Message to Front End
+            // Alternative display message here. Up to specific use case.
+            return new ViewModel(array('errorMsg' => $tweetMap->verifyCredentials(),'map_lat' => $tweetMap->get_map_lat(), 'map_long' => $tweetMap->get_map_long()));
+        }
+
+        $markers = $tweetMap->get_GeocodedTweets($address); // Get an Array of GeoCoded Tweets, formatted to be used as Google Maps Markers
+            
     }
 }
